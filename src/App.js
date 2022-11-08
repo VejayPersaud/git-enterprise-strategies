f36748d73d681c3b6934be2625c6fed85694b7ab
samilieberman <samilieberman1@gmail.com>
2022-11-07 20:54:51 -0500

    added new project text

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import gatorLogo from './gator.gif';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={gatorLogo} className="App-logo" alt="logo" />
-        <p> Hello World! </p>
-        <p> My name is Vejay Persaud and this is my project </p>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={gatorLogo} className="App-logo" alt="logo" />
+                <p>
+                    Hello World! My name is Vejay Persaud and this is our team project.
+                </p>
+            </header>
+        </div>
+    );
 }
 
 export default App;
